392ed1facd68d2d6dfb2442ab4b23adfa53ed240
James Scott <necr0mstr@gmail.com>
2025-03-21 20:44:44 -0400

    March backup

---
+++ 2025-02-20/backup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+BACKUPDIR="$HOME/git-files/Voron-300" 
+mkdir -p ${BACKUPDIR}/`date +%F`/ 
+rsync -avHl --exclude 'gcodes/*' --exclude 'logs/*' $HOME/printer_data/ ${BACKUPDIR}/`date +%F`/ | tee -a $HOME/logs/klipper_backup_`date +%F`.log  
+rsync -avHl $HOME/mainsail-config/client.cfg ${BACKUPDIR}/`date +%F`/client.cfg-`date +%F` | tee -a $HOME/logs/klipper_backup_`date +%F`.log
